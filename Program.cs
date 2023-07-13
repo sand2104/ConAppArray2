26083dccd6d932f828baea4f81542fb04a638574
sand2104 <prasadsandy2000@gmail.com>
2023-07-13 11:30:23 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppArray2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] myRectArray = new string[4, 3];
+            for (int i = 0; i < myRectArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myRectArray.GetLength(1); j++)
+                {
+                    myRectArray[i, j] = "[" + i + "," + j + "]";
+                }
+            }
+            Console.WriteLine("Stored Values are:");
+            for (int i = 0; i < myRectArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myRectArray.GetLength(1); j++)
+                {
+                    Console.WriteLine(myRectArray[i, j] + "\t");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
